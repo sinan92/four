c4a10b6c7f4349c0161d1cbd86e0d6e287baf964
Sinan <sinan113@hotmail.com>
2024-01-15 23:07:41 +0100

    Add light/dark mode

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log ("test")
+document.addEventListener('DOMContentLoaded', function () {
+    const body = document.body;
+    const modeToggle = document.getElementById('modeToggle');
+
+    modeToggle.addEventListener('click', function () {
+        body.classList.toggle('light-mode');
+        body.classList.toggle('dark-mode');
+    });
+});
