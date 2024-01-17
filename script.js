46df32c2f3e62fc04f6827e11f89d9faee7a7a34
Sinan <sinan113@hotmail.com>
2024-01-17 22:44:43 +0100

    Added swiper.js, products side by side, checkbox fix, dom manipulation

--- script.js
+++ script.js
@@ -6,4 +6,35 @@ document.addEventListener('DOMContentLoaded', function () {
         body.classList.toggle('light-mode');
         body.classList.toggle('dark-mode');
     });
+
+    new Swiper(".swiper-standard", {
+        slidesPerView: 1.5,
+        spaceBetween: 5,
+        keyboard: {
+            enabled: true,
+        },
+    });
+
+    new Swiper(".swiper-popular", {
+        slidesPerView: 1.2,
+        spaceBetween: 5,
+        pagination: {
+            el: ".swiper-pagination",
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        keyboard: {
+            enabled: true,
+        },
+    });
+
+    new Swiper(".swiper-highlights", {
+        slidesPerView: 1.2,
+        spaceBetween: 5,
+        keyboard: {
+            enabled: true,
+        },
+    });
 });
